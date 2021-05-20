d43ec243ba9fe4be5aa9216ec20ef51412da946b
Suto, Evelyne <evelyne.suto@garmin.com>
2021-05-20 17:43:30 +0300

    Initial Commit

---
+++ MovieProject/MovieProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieProjectApp.swift
+//  MovieProject
+//
+//  Created by Suto, Evelyne on 20/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct MovieProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
